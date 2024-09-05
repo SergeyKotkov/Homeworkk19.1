156a609054a260599a01102c3ac563f41b894370
XE <Smoke69rus@rambler.ru>
2024-09-05 19:59:25 +0300

    Правка файлов для вывода одного товара

--- Catalog/urls.py
+++ Catalog/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
@@ -7,6 +9,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
     path('Category_list/', views.catalog, name='Category_list'),
-    path('Category_detail/<int:pk>/', views.Category_detail, name='Category_detail')
-]
+    path('Category_detail/<int:pk>', views.Category_detail, name='Category_detail')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
