72622d0328cd942f01123116776649e2711215f1
XE <Smoke69rus@rambler.ru>
2024-09-10 21:52:12 +0300

    добавлен метод определения блога по публикации

--- Catalog/views.py
+++ Catalog/views.py
@@ -44,6 +44,11 @@ class contactsTemplateView(TemplateView):
 class BlogListView(ListView):
     model = Blog
 
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+        return queryset
+
 
 class BlogDetailView(DetailView):
     model = Blog
